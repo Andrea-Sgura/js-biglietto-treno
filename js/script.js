4ae9110a818930bffea38637ebf776d9a9778d48
Andrea-Sgura <andreasgura96@gmail.com>
2025-03-18 02:23:45 +0100

    [UPD] Created prompt with km and age

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+//1 - Definisco il prezzo del biglietto in base km (0.21 € al km)
 const costoPerkm = 0.21;
 let km = 1;
 let priceTicket = km * costoPerkm;
+
+//2 - Permetto all'utente di inserire il numero di chilometri che vuole percorrere.
 let kmDapercorrere = prompt("Inserisci i km da percorrere")
+
+//3 - Permetto all'utente di inserire la propria età.
 let etaUtente = prompt("Inserisci l'età")
+
