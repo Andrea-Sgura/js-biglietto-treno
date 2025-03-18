81ce2fa994186edd6c56a6a659bb442f6dfbd0f4
Andrea-Sgura <andreasgura96@gmail.com>
2025-03-18 02:34:54 +0100

    [UPD] Defined age discount

--- js/script.js
+++ js/script.js
@@ -9,3 +9,17 @@ let kmDapercorrere = prompt("Inserisci i km da percorrere")
 //3 - Permetto all'utente di inserire la propria età.
 let etaUtente = prompt("Inserisci l'età")
 
+//4 - Applico gli sconti in base all'età
+if (etaUtente <= 18){
+    sconto = 0.2;
+}
+
+else if (etaUtente >= 65){
+    sconto = 0.4;
+}
+
+else{
+    sconto = 0;
+}
+
+
